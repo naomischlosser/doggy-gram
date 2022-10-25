39f392445687c99f45e1e537e44dad245d2951db
naomischlosser <naomi_schlosser@hotmail.com>
2022-10-25 11:50:22 +0100

    add prettier to main folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import express, {Request,Response,Application} from 'express';
+import express, { Request, Response, Application } from "express";
 
-const app:Application = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req:Request, res:Response):void => {
-  res.send("Hello Typescript with Node.js!")
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Hello Typescript with Node.js!");
 });
 
-app.listen(PORT, ():void => {
-  console.log(`Server running here http://localhost:${PORT}`)
+app.listen(PORT, (): void => {
+  console.log(`Server running here http://localhost:${PORT}`);
 });
